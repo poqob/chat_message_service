4e2cb6153c4f8aa30b12ef7a54324da71ab0c08f
Mustafa <mmermmus123@gmail.com>
2023-03-30 01:43:21 +0300

    test file tested :DD

--- src/test/java/com/message_service/message_service/MessageServiceApplicationTests.java
+++ src/test/java/com/message_service/message_service/MessageServiceApplicationTests.java
@@ -3,11 +3,15 @@ package com.message_service.message_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.db.PostgreSQLJDBCConnection;
+
 @SpringBootTest
 class MessageServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PostgreSQLJDBCConnection c = new PostgreSQLJDBCConnection();
+		c.getChat();
 	}
 
 }
