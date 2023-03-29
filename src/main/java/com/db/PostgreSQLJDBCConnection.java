0e18bf5d80c488673fb15c2b2cc6e92229fa055b
Mustafa <mmermmus123@gmail.com>
2023-03-30 01:43:04 +0300

    getChat() added

--- src/main/java/com/db/PostgreSQLJDBCConnection.java
+++ src/main/java/com/db/PostgreSQLJDBCConnection.java
@@ -4,48 +4,91 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PostgreSQLJDBCConnection {
+import com.model.MessageModel;
+
+public class PostgreSQLJDBCConnection implements IDb {
 
     /*
      * beklentim: mesaj gönderimi, tüm mesajları çekmek, tarihe göre tek tek mesaj
      * çekmek.
      * 
      */
+    String dbURL = "jdbc:postgresql://localhost:5432/kanal";
 
-    public static void main(String[] args) {
+    // Veritabanı kullanıcı adı ve şifresi
+    String user = "java";
+    String password = "12345";
+    Connection conn;
 
-        // Veritabanı URL'si
-        String dbURL = "jdbc:postgresql://localhost:5432/kanal";
+    boolean isConnected() {
 
-        // Veritabanı kullanıcı adı ve şifresi
-        String user = "java";
-        String password = "12345";
+        boolean status = false;
 
         try {
-            // Veritabanına bağlantı oluştur
-            Connection conn = DriverManager.getConnection(dbURL, user, password);
+            status = conn.isValid(3);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return status;
 
-            // Bağlantının başarılı olduğunu kontrol et
+    }
+
+    public PostgreSQLJDBCConnection() {
+        // Veritabanı URL'si
+        try {
+            // Veritabanına bağlantı oluştur
+            conn = DriverManager.getConnection(dbURL, user, password);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<MessageModel> getChat() {
+        List<MessageModel> list = new ArrayList<MessageModel>();
+
+        try {
+
             if (conn != null) {
                 java.sql.Statement stmt = conn.createStatement();
                 String sql = "select * from canal.getconservation()";
 
-                System.out.println("Veritabanına başarıyla bağlandı.");
                 ResultSet res = stmt.executeQuery(sql);
+                MessageModel model;
+                while (res.next()) {
+                    model = new MessageModel(res.getInt("id_"), res.getString("username_"),
+                            res.getString("message_"),
+                            res.getDate("date_"), res.getString("info_"));
+                    list.add(model);
+                }
+                res.close();
 
-                MessageModel model = new MessageModel(res);
-
-                System.out.print(model);
                 // Statement nesnesini kapat
                 stmt.close();
 
                 // Veritabanı bağlantısını kapat
                 conn.close();
             }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
         }
+        return list;
+    }
+
+    @Override
+    public MessageModel getMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
     }
 }
