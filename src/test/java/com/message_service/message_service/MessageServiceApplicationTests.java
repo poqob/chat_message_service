1b8551b897d09bc7dda2bfcf7cfac22072da008b
Mustafa <mmermmus123@gmail.com>
2023-03-30 03:12:47 +0300

    test file

--- src/test/java/com/message_service/message_service/MessageServiceApplicationTests.java
+++ src/test/java/com/message_service/message_service/MessageServiceApplicationTests.java
@@ -1,17 +1,24 @@
 package com.message_service.message_service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.db.Db;
 import com.db.PostgreSQLJDBCConnection;
+import com.google.gson.Gson;
+import com.model.MessageModel;
 
 @SpringBootTest
 class MessageServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
-		PostgreSQLJDBCConnection c = new PostgreSQLJDBCConnection();
-		c.getChat();
+		Db db = Db.Db();
+		List<MessageModel> d = db.getChat();
+		String json = new Gson().toJson(d);
+		System.out.print(json);
 	}
 
 }
