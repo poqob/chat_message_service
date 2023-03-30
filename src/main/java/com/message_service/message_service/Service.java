2ebc940a0bc1e5f2b5d63ea9031398b173ae5ba2
Mustafa <mmermmus123@gmail.com>
2023-03-30 03:12:31 +0300

    url created and db connection provided

--- src/main/java/com/message_service/message_service/Service.java
+++ src/main/java/com/message_service/message_service/Service.java
@@ -1,15 +1,30 @@
 package com.message_service.message_service;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/test")
-public class Service {
+import com.db.Db;
+import com.google.gson.Gson;
+import com.model.MessageModel;
 
-    @GetMapping
+@RestController
+@RequestMapping(value = { "/conservation", "/last" })
+public class Service {
+    List<MessageModel> list;
+
+    @GetMapping(name = "/conservation")
     public String getAllMessages() {
-        return "esenlikler dilerim";
+        if (list == null)
+            list = Db.Db().getChat();
+        String json = new Gson().toJson(list);
+        return json;
+    }
+
+    @GetMapping(name = "/last")
+    public String getMessagee() {
+        return "last message";
     }
 }
